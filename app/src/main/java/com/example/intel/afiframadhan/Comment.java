5684c37bd1bf9319e105915cad40f2b874b25094
afiframadhan98 <afiframadhan231@gmail.com>
2018-04-01 21:42:45 +0700

    first commit

---
+++ app/src/main/java/com/example/intel/afiframadhan/Comment.java
@@ -0,0 +1,31 @@
+package com.example.intel.afiframadhan;
+
+public class Comment {
+    String postId;
+    String username;
+    String message;
+
+    public String getPostId() {
+        return postId;
+    }
+
+    public void setPostId(String postId) {
+        this.postId = postId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
